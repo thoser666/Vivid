6bcaf0e56f975b3b27cdc95c956b9b7a95cc7e53
stbrumm <thoser666@web.de>
2026-01-26 11:53:11 +0100

    adding jitpack

--- feature-streaming/build.gradle.kts
+++ feature-streaming/build.gradle.kts
@@ -49,9 +49,10 @@ dependencies {
     // HIER SIND DIE KORREKTEN, AUFGETEILTEN ABHÄNGIGKEITEN
     // WIE IM GITHUB-ISSUE BESCHRIEBEN.
     // 'ConnectCheckerRtmp' ist im ':rtmp'-Modul.
-    implementation("com.github.pedroSG94.RootEncoder:encoder:2.6.4")
-    implementation("com.github.pedroSG94.RootEncoder:library:2.6.4")
-    implementation("com.github.pedroSG94.RootEncoder:rtmp:2.6.4")
+    implementation("com.github.pedroSG94:encoder:2.6.4")
+    implementation("com.github.pedroSG94:library:2.6.4")
+    implementation("com.github.pedroSG94:rtmp:2.6.4")
+    implementation("com.github.pedroSG94:gl:2.6.4")
     // =============================================================
 
     // Media3 / ExoPlayer für die Wiedergabe
